efe6ca394ec298c16a91949e3264a9c1bb55edce
ASOST\2316016 <jyonnai777@gmail.com>
2025-04-15 10:40:57 +0900

    item

--- Src/Manager/Game/ItemManager.h
+++ Src/Manager/Game/ItemManager.h
@@ -1,8 +1,19 @@
 #pragma once
+#include <DxLib.h>
+#include <vector>
+#include "../../Common/Quaternion.h"
+#include "../../Common/IntVector3.h"
+#include "../../Object/Item/ItemBase.h"
 class ItemManager
 {
 public:
+
+
+
 	void Init(void);
+
+	void AddItem(IntVector3 mapPos,Quaternion rot,ItemBase::ITEM_TYPE type);				//アイテム追加　ステータス
+	void DeleteItem(VECTOR mapPos, int range);	//アイテム消去　消去の中心地, 消去の範囲
 
 	// 明示的にインステンスを生成する
 	static void CreateInstance(void);
@@ -13,6 +24,9 @@ protected:
 
 private:
 	static ItemManager* instance_;
+
+	std::vector<ItemBase*> items_;
+
 
 	ItemManager(void);
 	ItemManager(const ItemManager& instance_) = default;
