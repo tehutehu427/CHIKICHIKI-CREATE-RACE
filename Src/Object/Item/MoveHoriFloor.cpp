dce1ee30687b42e5799d774b60ad5c0ad30653f8
tehutehu <nody0126nody@docomo.ne.jp>
2025-04-16 15:56:26 +0900

    横移動床製作中

---
+++ Src/Object/Item/MoveHoriFloor.cpp
@@ -0,0 +1,47 @@
+#include "../Utility/Utility.h"
+#include "MoveHoriFloor.h"
+
+MoveHoriFloor::MoveHoriFloor()
+{
+}
+
+MoveHoriFloor::~MoveHoriFloor()
+{
+}
+
+void MoveHoriFloor::Load(void)
+{
+}
+
+void MoveHoriFloor::Init(void)
+{
+	//å¬ÅXÇÃê›íË
+	SetParam();
+}
+
+void MoveHoriFloor::SetParam(void)
+{
+	size_ = MAP_SIZE;
+	status_.isBreak = true;
+	status_.isGravity = false;
+	status_.itemType = ITEM_TYPE::FLOOR;
+	status_.effType = EFFECT_TYPE::INSTALLATION;
+}
+
+void MoveHoriFloor::Update(void)
+{
+	
+}
+
+void MoveHoriFloor::Draw(void)
+{
+}
+
+void MoveHoriFloor::Release(void)
+{
+}
+
+void MoveHoriFloor::Move(void)
+{
+	//VECTOR movePow = Utility::GetMoveVec(transform_.pos, VAdd(transform_.pos, VGet(size_.x * 100, size_.y * 100, size_.z * 100)));
+}
