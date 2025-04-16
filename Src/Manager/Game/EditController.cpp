8fd86f60572e2699f14cb744a81dac7a653f66c7
ASOST\2316016 <jyonnai777@gmail.com>
2025-04-16 11:12:21 +0900

    item

--- Src/Manager/Game/EditController.cpp
+++ Src/Manager/Game/EditController.cpp
@@ -1,1 +1,26 @@
+#include "../System/InputManager.h"
 #include "EditController.h"
+
+void EditController::Init(void)
+{
+}
+
+void EditController::Update(void)
+{
+	mousePos2D_ = InputManager::GetInstance().GetMousePos();
+	ChengePos2Dfor3D();
+
+}
+
+void EditController::Draw(void)
+{
+}
+
+void EditController::Release(void)
+{
+}
+
+void EditController::ChengePos2Dfor3D(void)
+{
+
+}
