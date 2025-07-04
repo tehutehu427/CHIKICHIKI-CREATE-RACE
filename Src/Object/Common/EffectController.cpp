9ddffe57048133a2f49d41fbaaca372aad749756
tehutehu <nody0126nody@docomo.ne.jp>
2025-07-04 17:57:27 +0900

    エフェクトどうしよう。。。

--- Src/Object/Common/EffectController.cpp
+++ Src/Object/Common/EffectController.cpp
@@ -4,21 +4,46 @@
 
 EffectController::EffectController(void)
 {
+	effectNum_ = 0;
 }
 
 EffectController::~EffectController(void)
 {
+	//エフェクトのクリア
+	effects_.clear();
 }
 
-void EffectController::Add()
+const int EffectController::Play(const int _effHandle, const VECTOR _pos, const Quaternion _quaRot, const VECTOR _scl, const float speedMultiplier)
 {
+	//エフェクトハンドル
+	Effect playEffect;
 
+	//プレイハンドルを設定
+	playEffect.playId = PlayEffekseer3DEffect(_effHandle);
+
+	//以降、再生するエフェクトの制御は必ずプレイハンドルIDを使用すること
+	//エフェクトの大きさ設定(XYZ)
+	SetScalePlayingEffekseer3DEffect(playEffect.playId, _scl.x, _scl.y, _scl.z);
+
+	//エフェクトの角度を設定
+	SetRotationPlayingEffekseer3DEffect(playEffect.playId, _quaRot.ToEuler().x, _quaRot.ToEuler().y, _quaRot.ToEuler().z);
+
+	//エフェクトの位置を設定
+	SetPosPlayingEffekseer3DEffect(playEffect.playId, _pos.x, _pos.y, _pos.z);	
+
+	//エフェクトの速度を設定
+	float normalSpeed = GetSpeedPlayingEffekseer3DEffect(playEffect.playId);
+	SetSpeedPlayingEffekseer3DEffect(playEffect.playId, normalSpeed / speedMultiplier);
+
+	//エフェクトの保存
+	effects_.emplace_back(playEffect);
+
+	//カウンタ増加
+	effectNum_++;
 }
 
-void EffectController::Play()
+void EffectController::Stop(const int _arrayNum)
 {
-}
-
-void EffectController::Stop()
-{
+	//エフェクトストップ
+	StopEffekseer3DEffect(effects_[_arrayNum].playId);
 }
--- Src/Object/Common/EffectController.h
+++ Src/Object/Common/EffectController.h
@@ -1,18 +1,18 @@
 #pragma once
 
+#include<vector>
+#include"../Common/Quaternion.h"
+
 class EffectController
 {
 public:
 
 	struct Effect
 	{
-		int resId;			//初期データ
 		int playId;			//再生データ
+		float speedPer;		//再生スピード割合
 		float time;			//再生時間
 		float playCount;	//再生カウンタ
-		VECTOR pos;			//表示座標
-		VECTOR rot;			//回転
-		VECTOR scl;			//大きさ
 	};
 
 	//コンストラクタ
@@ -21,14 +21,32 @@ public:
 	//デストラクタ
 	~EffectController(void);
 
-	void Add();
+	/// <summary>
+	/// エフェクトの再生
+	/// </summary>
+	/// <param name="_effHandle">エフェクトハンドル</param>
+	/// <param name="_pos">座標情報</param>
+	/// <param name="_quaRot">回転情報</param>
+	/// <param name="_scl">大きさ情報</param>
+	/// <param name="speedMultiplier">速度の倍率</param>
+	/// <returns>再生したエフェクトの配列番号(このエフェクトの設定をいじる時に使用)</returns>
+	const int Play(const int _effHandle,
+		const VECTOR _pos,
+		const Quaternion _quaRot,
+		const VECTOR _scl,
+		const float speedMultiplier = NORMAL_MULTI);
 
-	void Play();
-
-	void Stop();
+	/// <summary>
+	/// 再生中エフェクトを止める
+	/// </summary>
+	/// <param name="_arrayNum">止めたいエフェクトの配列番号</param>
+	void Stop(const int _arrayNum);
 
 private:
 
-	std::map<int, Effect> effects_;		//エフェクト関連
+	static constexpr float NORMAL_MULTI = 1.0f;	//通常倍率
+
+	int effectNum_;					//プレイ中のエフェクトの数
+	std::vector<Effect> effects_;	//エフェクト関連
 };
 
