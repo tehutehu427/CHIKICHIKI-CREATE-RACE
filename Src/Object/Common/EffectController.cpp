a603ca3d81a659b118b8817036d12d21dbb4d8c2
tehutehu <nody0126nody@docomo.ne.jp>
2025-07-07 11:00:12 +0900

    とりあえず仮で

--- Src/Object/Common/EffectController.cpp
+++ Src/Object/Common/EffectController.cpp
@@ -4,46 +4,88 @@
 
 EffectController::EffectController(void)
 {
-	effectNum_ = 0;
 }
 
 EffectController::~EffectController(void)
 {
 	//エフェクトのクリア
 	effects_.clear();
+	effectNum_.clear();
 }
 
-const int EffectController::Play(const int _effHandle, const VECTOR _pos, const Quaternion _quaRot, const VECTOR _scl, const float speedMultiplier)
+void EffectController::Add(const int _effHandle, const EFF_TYPE _type)
 {
-	//エフェクトハンドル
-	Effect playEffect;
+	//すでに入ってるなら何もしない
+	if (effects_[_type].resId != -1)return;
+
+	//エフェクト
+	EffectData effect;
+
+	//リソースID
+	effect.resId = _effHandle;
+
+	//追加
+	effects_.emplace(_type, effect);
+	effectNum_.emplace(_type, 0);
+}
+
+const int EffectController::Play(const EFF_TYPE _effType, const VECTOR _pos, const Quaternion _quaRot, const VECTOR _scl, const float _speedMultiplier)
+{
+	//中身がないなら失敗
+	if (effects_[_effType].resId == -1)return -1;
 
 	//プレイハンドルを設定
-	playEffect.playId = PlayEffekseer3DEffect(_effHandle);
+	int playId = PlayEffekseer3DEffect(effects_[_effType].resId);
 
 	//以降、再生するエフェクトの制御は必ずプレイハンドルIDを使用すること
 	//エフェクトの大きさ設定(XYZ)
-	SetScalePlayingEffekseer3DEffect(playEffect.playId, _scl.x, _scl.y, _scl.z);
+	SetScalePlayingEffekseer3DEffect(playId, _scl.x, _scl.y, _scl.z);
 
 	//エフェクトの角度を設定
-	SetRotationPlayingEffekseer3DEffect(playEffect.playId, _quaRot.ToEuler().x, _quaRot.ToEuler().y, _quaRot.ToEuler().z);
+	SetRotationPlayingEffekseer3DEffect(playId, _quaRot.ToEuler().x, _quaRot.ToEuler().y, _quaRot.ToEuler().z);
 
 	//エフェクトの位置を設定
-	SetPosPlayingEffekseer3DEffect(playEffect.playId, _pos.x, _pos.y, _pos.z);	
+	SetPosPlayingEffekseer3DEffect(playId, _pos.x, _pos.y, _pos.z);	
 
 	//エフェクトの速度を設定
-	float normalSpeed = GetSpeedPlayingEffekseer3DEffect(playEffect.playId);
-	SetSpeedPlayingEffekseer3DEffect(playEffect.playId, normalSpeed / speedMultiplier);
+	effects_[_effType].normalSpeed = GetSpeedPlayingEffekseer3DEffect(playId);
+	SetSpeedPlayingEffekseer3DEffect(playId, effects_[_effType].normalSpeed * _speedMultiplier);
 
 	//エフェクトの保存
-	effects_.emplace_back(playEffect);
+	effects_[_effType].playId.emplace_back(playId);
+
+	//配列保存
+	int ret = effectNum_[_effType];
 
 	//カウンタ増加
-	effectNum_++;
+	effectNum_[_effType]++;
+
+	//配列番号を返す
+	return ret;
 }
 
-void EffectController::Stop(const int _arrayNum)
+void EffectController::SetPos(const EFF_TYPE _effType, const int _arrayNum, const VECTOR _pos)
+{
+	SetPosPlayingEffekseer3DEffect(effects_[_effType].playId[_arrayNum], _pos.x, _pos.y, _pos.z);
+}
+
+void EffectController::SetQuaRot(const EFF_TYPE _effType, const int _arrayNum, const Quaternion _quaRot)
+{
+	SetRotationPlayingEffekseer3DEffect(effects_[_effType].playId[_arrayNum], _quaRot.ToEuler().x, _quaRot.ToEuler().y, _quaRot.ToEuler().z);
+}
+
+void EffectController::SetScale(const EFF_TYPE _effType, const int _arrayNum, const VECTOR _scl)
+{
+	SetScalePlayingEffekseer3DEffect(effects_[_effType].playId[_arrayNum], _scl.x, _scl.y, _scl.z);
+}
+
+void EffectController::SetSpeed(const EFF_TYPE _effType, const int _arrayNum, const float _speedMultiplier)
+{
+	SetSpeedPlayingEffekseer3DEffect(effects_[_effType].playId[_arrayNum], effects_[_effType].normalSpeed * _speedMultiplier);
+}
+
+void EffectController::Stop(const EFF_TYPE _effType, const int _arrayNum)
 {
 	//エフェクトストップ
-	StopEffekseer3DEffect(effects_[_arrayNum].playId);
+	StopEffekseer3DEffect(effects_[_effType].playId[_arrayNum]);
 }
--- Src/Object/Common/EffectController.h
+++ Src/Object/Common/EffectController.h
@@ -7,12 +7,11 @@ class EffectController
 {
 public:
 
-	struct Effect
+	//エフェクトの種類
+	enum class EFF_TYPE
 	{
-		int playId;			//再生データ
-		float speedPer;		//再生スピード割合
-		float time;			//再生時間
-		float playCount;	//再生カウンタ
+		NONE,
+		WIND,		//風
 	};
 
 	//コンストラクタ
@@ -22,31 +21,79 @@ public:
 	~EffectController(void);
 
 	/// <summary>
-	/// エフェクトの再生
+	/// エフェクトの追加
 	/// </summary>
 	/// <param name="_effHandle">エフェクトハンドル</param>
+	/// <param name="_type">ハンドルを保存したいタイプ</param>
+	void Add(const int _effHandle, const EFF_TYPE _type);
+
+	/// <summary>
+	/// エフェクトの再生
+	/// </summary>
+	/// <param name="_effType">エフェクトの種類</param>
 	/// <param name="_pos">座標情報</param>
 	/// <param name="_quaRot">回転情報</param>
 	/// <param name="_scl">大きさ情報</param>
 	/// <param name="speedMultiplier">速度の倍率</param>
-	/// <returns>再生したエフェクトの配列番号(このエフェクトの設定をいじる時に使用)</returns>
-	const int Play(const int _effHandle,
+	/// <returns>再生したエフェクトの配列番号(このエフェクトの設定をいじる時に使用), -1:失敗</returns>
+	const int Play(const EFF_TYPE _effType,
 		const VECTOR _pos,
 		const Quaternion _quaRot,
 		const VECTOR _scl,
-		const float speedMultiplier = NORMAL_MULTI);
+		const float _speedMultiplier = NORMAL_MULTI);
+
+	/// <summary>
+	/// 座標の再設定
+	/// </summary>
+	/// <param name="_effType">エフェクトの種類</param>
+	/// <param name="_arrayNum">変更したいエフェクトの配列番号</param>
+	/// <param name="_pos">変更後の座標情報</param>
+	void SetPos(const EFF_TYPE _effType, const int _arrayNum, const VECTOR _pos);
+	
+	/// <summary>
+	/// 回転の再設定
+	/// </summary>
+	/// <param name="_effType">エフェクトの種類</param>
+	/// <param name="_arrayNum">変更したいエフェクトの配列番号</param>
+	/// <param name="_quaRot">変更後の回転情報</param>
+	void SetQuaRot(const EFF_TYPE _effType, const int _arrayNum, const Quaternion _quaRot);
+	
+	/// <summary>
+	/// 大きさの再設定
+	/// </summary>
+	/// <param name="_effType">エフェクトの種類</param>
+	/// <param name="_arrayNum">変更したいエフェクトの配列番号</param>
+	/// <param name="_scl">変更後の大きさ</param>
+	void SetScale(const EFF_TYPE _effType, const int _arrayNum, const VECTOR _scl);
+
+	/// <summary>
+	/// 速度の再設定
+	/// </summary>
+	/// <param name="_effType">エフェクトの種類</param>
+	/// <param name="_arrayNum">変更したいエフェクトの配列番号</param>
+	/// <param name="_speed">変更後の速度倍率</param>
+	void SetSpeed(const EFF_TYPE _effType, const int _arrayNum, const float _speedMultiplier);
 
 	/// <summary>
 	/// 再生中エフェクトを止める
 	/// </summary>
+	/// <param name="_effType">止めたいエフェクトの種類</param>
 	/// <param name="_arrayNum">止めたいエフェクトの配列番号</param>
-	void Stop(const int _arrayNum);
+	void Stop(const EFF_TYPE _effType, const int _arrayNum);
 
 private:
 
+	//エフェクトデータ
+	struct EffectData
+	{
+		int resId;					//リソースデータ
+		std::vector<int> playId;	//再生データ
+		float normalSpeed;			//通常速度
+	};
+
 	static constexpr float NORMAL_MULTI = 1.0f;	//通常倍率
 
-	int effectNum_;					//プレイ中のエフェクトの数
-	std::vector<Effect> effects_;	//エフェクト関連
+	std::map<EFF_TYPE, int> effectNum_;			//プレイ中のエフェクトの数
+	std::map<EFF_TYPE, EffectData> effects_;	//エフェクト関連
 };
 
