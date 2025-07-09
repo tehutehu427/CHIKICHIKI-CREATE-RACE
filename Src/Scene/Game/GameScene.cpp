dd667d4e629b54c64564cbc8bbb44d0aa513cbfe
ASOST\2316016 <jyonnai777@gmail.com>
2025-07-09 10:08:19 +0900

    playerの処理

--- Src/Scene/Game/GameScene.cpp
+++ Src/Scene/Game/GameScene.cpp
@@ -331,11 +331,25 @@ void GameScene::DrawEdit(void)
 
 void GameScene::DrawAction(void)
 {
+	auto screenIndex = SceneManager::GetInstance().GetScreenIndex();
 	//アイテム
 	ItemManager::GetInstance().Draw();
 	//プレイヤー
 	PlayerManager::GetInstance().Draw();
 
+	if (PlayerManager::GetInstance().IsPlayerDeath(screenIndex))
+	{
+		SetDrawBlendMode(DX_BLENDMODE_ALPHA, 128);
+		DrawBox(0, 0, Application::SCREEN_SIZE_X, Application::SCREEN_SIZE_Y, Utility::BLACK, true);
+		SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
+	}
+	else if (PlayerManager::GetInstance().IsPlayerGoal(screenIndex))
+	{
+		SetDrawBlendMode(DX_BLENDMODE_ALPHA, 128);
+		DrawBox(0, 0, Application::SCREEN_SIZE_X, Application::SCREEN_SIZE_Y, Utility::YELLOW, true);
+		SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
+	}
+	
 }
 
 void GameScene::DrawClear()
