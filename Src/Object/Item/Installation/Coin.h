70ba0e23a2e7f623cacdd16490be097909fc8358
tehutehu <nody0126nody@docomo.ne.jp>
2025-07-22 15:53:51 +0900

    coin作成中

---
+++ Src/Object/Item/Installation/Coin.h
@@ -0,0 +1,41 @@
+#pragma once
+#include "../ItemBase.h"
+
+class Coin : public ItemBase
+{
+public:
+
+	//マップ
+	static constexpr IntVector3 MAP_SIZE = { 1,1,1 };				//マップサイズ
+	static constexpr VECTOR MAP_LOCALPOS = { 10.0f,10.0f,10.0f };	//マップとの相対座標
+
+	//サイズ
+	static constexpr VECTOR MODEL_SIZE = { 220.0f,140.0f,20.0f };	//モデルのサイズ
+
+	//追従
+	static constexpr VECTOR FOLLOW_LOCAL_POS = { 0.0f,100.0f,-100.0f };		//追従座標
+
+	//コンストラクタ
+	Coin(void);
+	
+	//デストラクタ
+	~Coin(void);
+
+	//個人設定
+	void SetParam(void)override;
+	//更新
+	void Update(void)override;
+	//描画
+	void Draw(void)override;
+
+	/// <summary>
+	/// 当たり判定後の処理
+	/// </summary>
+	/// <param name="_hitColTag">相手側の当たり判定</param>
+	void OnHit(const std::weak_ptr<Collider> _hitCol)override;
+
+private:
+
+	std::weak_ptr<Collider> followCol_;		//ついていく対象
+};
+
